5c58a438d4c47c9648ab96797ea350c020eacf22
anotherITdude <abhi.baidya@gmail.com>
2023-05-03 20:57:46 +0200

    fix: dynamic route fix - 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { AppProps } from "next/app";
 import { StyleProvider, ThemePicker } from "vcc-ui";
 import "../public/css/styles.css";
-import Home from "./index";
+import Home from "./Home";
 
 const HomePage = ({ Component, pageProps }: AppProps) => {
   const newLocal = <Component {...pageProps} />;
