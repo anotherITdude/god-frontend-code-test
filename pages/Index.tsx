a655ec70c072bcf7548dde23f6aefaf33001ce3f
anotherITdude <abhi.baidya@gmail.com>
2023-05-03 20:06:27 +0200

    fix: layout fix

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { NextPage } from "next";
 import Carousel from "../src/components/Carousel/Main";
-import Navbar from '../src/components/Navbar';
+import Navbar from "../src/components/Navbar";
 
-const Home: NextPage = () => {
+const Home: React.FC = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Carousel />
     </>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-const Document = () => {
+export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-white">
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-};
-
-export default Document;
+}
